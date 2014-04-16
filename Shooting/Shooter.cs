ac4e36379b08441832db16614e72190a516f5f91
Evgeniy Plyusnin <eninbox@gmail.com>
2014-04-16 11:59:59 +0600

    Первая работающая версия на основе WinForms, но без WinForms

---
+++ Shooting/Shooter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Shooting
+{
+    public class Shooter
+    {
+        public static Bitmap MakeScreenShot(Screen screen)
+        {
+            try
+            {
+                Bitmap bitmap = new Bitmap(screen.Bounds.Width, screen.Bounds.Height);
+
+                using (Graphics gfx = Graphics.FromImage(bitmap))
+                {
+                    gfx.CopyFromScreen(0, 0, 0, 0, new Size(bitmap.Width, bitmap.Height));
+                    gfx.Dispose();
+                }
+                return bitmap;
+            }
+            catch (Exception exc)
+            {
+                //using (Graphics gfx = Graphics.FromImage(bitmap))
+                //{
+                //    gfx.DrawString("Error:\r\n" + exc.ToString(), new Font("arial", 12), Brushes.Black, new PointF(0, 0));
+                //}
+            }
+            return null;
+        }
+    }
+}
---
+++ webShooter/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Shooting;
+
+namespace webShooter
+{
+    static class Program
+    {
+        private static readonly CancellationTokenSource TokenSource = new CancellationTokenSource();
+
+        [STAThread]
+        static void Main()
+        {
+            ListeningLoop();
+
+            Application.Run();
+        }
+
+        private static async void ListeningLoop()
+        {
+            var listener = new HttpListener();
+            listener.Prefixes.Add("http://+:4444/");
+            listener.IgnoreWriteExceptions = true;
+            listener.Start();
+            while (true)
+            {
+                var context = await listener.GetContextAsync();
+                try
+                {
+                    if (context.Request.Url.LocalPath == "/screen.png")
+                    {
+                        var screenshot = Shooter.MakeScreenShot(Screen.PrimaryScreen);
+                        screenshot.Save(context.Response.OutputStream, ImageFormat.Png);
+                    }
+                }
+                finally
+                {
+                    context.Response.Close();
+                }
+            }
+        }
+    }
+}
